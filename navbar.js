f70a7f639430043777cfaa74ace0518a9bf94762
Jason Gao <jgao2299@gmail.com>
2020-01-06 10:21:58 -0500

    updated the site

--- navbar.js
+++ navbar.js
@@ -1,11 +1,27 @@
-var navItem = document.querySelectorAll(".nav-item");
 
-for (var i = 0; i < navItem.length; i++) {
-    navItem[i].addEventListener("mouseover", function() {
-        this.classList.add("hover");
-    })
 
-    navItem[i].addEventListener("mouseleave", function() {
-        this.classList.remove("hover");
-    })
-}   
+// var firstTab = document.getElementById("About");
+// var secondTab = document.getElementById("Experience");
+// var thirdTab = document.getElementById("Project");
+
+// var navItem = document.querySelectorAll(".nav-item");
+// var home = navItem[0];
+// var exp = navItem[1];
+// var proj = navItem[2];
+
+// var navLinks = document.querySelectorAll(".nav-link");
+// var tab = "";
+
+// navLinks[0].addEventListener("click", function() {
+//     tab = this.textContent;
+//     console.log(tab)   
+// })
+ 
+
+// if (firstTab.textContent === "About") {
+//     home.classList.add("active")
+// } else if (secondTab.textContent === "Work Experience") {
+//     exp.classList.add("active")
+// } else {
+//     proj.classList.add("active")
+// }
